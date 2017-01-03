1e88dc32197a635aaca9977332c7012a274961dc
devincarl <devincarl@aliyun.com>
2017-01-03 22:48:14 +0800

    orientation

---
+++ blog/Orientation/js/orientation.js
@@ -0,0 +1,19 @@
+$(function () {
+  $('.vr-img').one('load', function() {
+    var hei = $('.vr-frame').height() + 200;
+    $(this).height(hei);
+    var wid = $(this).width();
+    $(this).css({
+      'marginLeft': -1*wid/2+'px',
+      'marginTop': -1*hei/2+'px',
+    });
+
+    var panorama = new dcPanorama('.vr-frame', '.vr-img',{
+      compassRange: 90,
+      betaRange: 30
+    })
+  }).each(function() {
+    if(this.complete) $(this).load();
+  });
+})
+
